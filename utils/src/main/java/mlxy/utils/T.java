8e9ee37694b3139f055a86c07fc41f83cf2d4457
mlxy <chihane@yeah.net>
2015-10-30 17:53:46 +0800

    immigrate to gradle

---
+++ utils/src/main/java/mlxy/utils/T.java
@@ -0,0 +1,25 @@
+package mlxy.utils;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/** Simplified {@link Toast}. */
+public class T {
+    /** Show long Toast.
+     *
+     *  @see Toast#makeText(Context, CharSequence, int)
+     *  @see Toast#LENGTH_LONG
+     */
+    public static void showLong(Context context, CharSequence text) {
+        Toast.makeText(context, text, Toast.LENGTH_LONG).show();
+    }
+
+    /** Show short Toast.
+     *
+     *  @see Toast#makeText(Context, CharSequence, int)
+     *  @see Toast#LENGTH_SHORT
+     */
+    public static void showShort(Context context, CharSequence text) {
+        Toast.makeText(context, text, Toast.LENGTH_SHORT).show();
+    }
+}
