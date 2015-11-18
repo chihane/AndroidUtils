e4211ae11421c691e9d3f7110cdbcf98e3c7b863
mlxy <chihane@yeah.net>
2015-11-18 13:49:09 +0800

    change dp2px as static

--- utils/src/main/java/mlxy/utils/Device.java
+++ utils/src/main/java/mlxy/utils/Device.java
@@ -3,7 +3,7 @@ package mlxy.utils;
 import android.content.Context;
 
 public class Device {
-    public int dp2px(Context context, float dp) {
+    public static int dp2px(Context context, float dp) {
         return (int) Math.ceil(context.getResources().getDisplayMetrics().density * dp);
     }
 }
