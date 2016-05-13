f69ca5d6825e1b3d3e2b2ef20ead884fe32668c6
chihane <chihane@yeah.net>
2016-05-13 09:20:45 +0800

    Lists.notEmpty(List)

--- utils/src/main/java/mlxy/utils/Lists.java
+++ utils/src/main/java/mlxy/utils/Lists.java
@@ -6,4 +6,8 @@ public class Lists {
     public static boolean isEmpty(List list) {
         return list == null || list.size() == 0;
     }
+
+    public static boolean notEmpty(List list) {
+        return list != null && list.size() > 0;
+    }
 }
