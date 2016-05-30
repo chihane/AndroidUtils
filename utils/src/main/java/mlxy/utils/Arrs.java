aa778ddbcb5429120fe416b7ca00f8a150f27848
chihane <chihane@yeah.net>
2016-05-30 15:08:32 +0800

    Add Arrs

---
+++ utils/src/main/java/mlxy/utils/Arrs.java
@@ -0,0 +1,11 @@
+package mlxy.utils;
+
+public class Arrs {
+    public static boolean isEmpty(Object[] array) {
+        return array == null || array.length == 0;
+    }
+
+    public static boolean notEmpty(Object[] array) {
+        return array != null && array.length > 0;
+    }
+}
