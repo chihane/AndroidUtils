47053c7af2fb00542bf43161663baca27ccb7212
chihane <chihane@yeah.net>
2016-07-13 10:39:36 +0800

    Dev.screenWidth Dev.screenHeight

--- utils/src/main/java/mlxy/utils/Dev.java
+++ utils/src/main/java/mlxy/utils/Dev.java
@@ -1,6 +1,7 @@
 package mlxy.utils;
 
 import android.content.Context;
+import android.view.WindowManager;
 
 public class Dev {
     public static int dp2px(Context context, float dp) {
@@ -9,5 +10,15 @@ public class Dev {
 
     public static int sp2px(Context context, float sp) {
         return (int) Math.ceil(context.getResources().getDisplayMetrics().scaledDensity * sp);
+    }
+
+    public static int screenWidth(Context context) {
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        return wm.getDefaultDisplay().getWidth();
+    }
+
+    public static int screenHeight(Context context) {
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        return wm.getDefaultDisplay().getHeight();
     }
 }
