38cb21d96dcc8fe6afb127fdeb2e6cb8aae92f2d
Chihane <chihane@yeah.net>
2017-06-19 11:15:59 +0800

    Add Network.available()

--- utils/src/main/java/mlxy/utils/Network.java
+++ utils/src/main/java/mlxy/utils/Network.java
@@ -4,8 +4,13 @@ import android.content.Context;
 import android.net.ConnectivityManager;
 
 public class Network {
-    public static boolean connected(Context context) {
+    public static boolean available(Context context) {
         ConnectivityManager connManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
         return connManager.getActiveNetworkInfo() != null && connManager.getActiveNetworkInfo().isAvailable();
+    }
+
+    public static boolean connected(Context context) {
+        ConnectivityManager connManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        return connManager.getActiveNetworkInfo() != null && connManager.getActiveNetworkInfo().isConnected();
     }
 }
