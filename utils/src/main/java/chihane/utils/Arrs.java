ba0d87787af31d7689b92008e587e144d7dda22b
Chihane <chihane@yeah.net>
2017-11-09 17:06:39 +0800

    Add lastItem() to Arrs

--- utils/src/main/java/chihane/utils/Arrs.java
+++ utils/src/main/java/chihane/utils/Arrs.java
@@ -8,4 +8,8 @@ public class Arrs {
     public static boolean notEmpty(Object[] array) {
         return array != null && array.length > 0;
     }
+
+    public static <I> I lastItem(I[] items) {
+        return isEmpty(items) ? null : items[items.length - 1];
+    }
 }
